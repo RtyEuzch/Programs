fdd35ac37eef62b4457b1bef7bc26fb7fcb87a84
RtyEuzch <137963453+RtyEuzch@users.noreply.github.com>
2023-12-21 20:06:10 -0600

    Added timer and its listener, along with button

--- BouncingBlockMain.java
+++ BouncingBlockMain.java
@@ -20,80 +20,129 @@ public class BouncingBlockMain {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         BouncingBlockComponent component = new BouncingBlockComponent();
         frame.add(component);
-        frame.setVisible(true);
         component.addButton(frame);
+        frame.setVisible(true);
     }
 }
 
+/**
+ * 
+ */
 class BouncingBlockComponent extends JComponent {
+    public static final int INCREMENT = 20;
+    public static final int DELAY = 10;
     public static final int X_POS = 10;
-    public static final int Y_POS = 2;   
-    public static int heightOffset;
-    @Override
-    public void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g;
-        //Adding the block
-        int recWidth = getWidth() / 4;
-        int recHeight = getHeight() / 3;
-        heightOffset = recHeight / 2;
-        int xCoord = getWidth() / X_POS;
-        int yCoord = getHeight() / Y_POS - heightOffset;
-        Block block = new Block(xCoord, yCoord, recWidth, recHeight);
-        block.draw(g2);
+    public static final int Y_POS = 2; 
+    private Block block;
+    private Timer t;
+    /**
+     * The constructor, which creates the block
+     */
+    public BouncingBlockComponent() {
+        block = new Block(); System.out.println("Construct a blcok");
     }
 
+    @Override
+    public void paintComponent(Graphics g) {System.out.println("Painted");
+        Graphics2D g2 = (Graphics2D) g;
+        //Adding the block
+        block.draw(g2);
+    }
+    /**
+     * 
+     */
     public void addButton(JFrame frame) {
         //Adding the button to start the movement of the block
         JButton button = new JButton("Start");
-        ActionListener bListen = new ActionListener() {
+        button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.println("Hey");
+                //System.out.println("Hey");
+                motionMover();
             }
-        };
-        button.addActionListener(bListen);
-        frame.add(button);
+        });
+
+        JPanel btnPanel = new JPanel();
+        btnPanel.add(button);
+        frame.add(btnPanel, BorderLayout.SOUTH);
+    }
+
+    public void motionMover() {
+        class TimerListener implements ActionListener {
+            private Block b;
+            public TimerListener(Block bl) { System.out.println("TimerListened");
+                b = bl;
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                b.move(INCREMENT); 
+            }
+        }
+        ActionListener timeListen = new TimerListener(block);
+        t = new Timer(DELAY, timeListen);
+        t.start(); System.out.println("Timer started");
+        
     }
 
     /** This class represents the block entity and manipulates its
      *  position.
      */  
-    class Block {           
+    class Block { 
+        public static final int REC_WIDTH = 4;
+        public static final int REC_HEIGHT = 3;   
+        public static double heightOffset;     
+        private double width;
+        private double height;    
         private double x;
         private double y;
-        private double width;
-        private double height;
         private Rectangle2D.Double rec;
-        public Block(double x, double y, double w, double h) {
-            this.x = x;
-            this.y = y;
-            width = w;
-            height = h;
-            rec = new Rectangle2D.Double(x, y, w, h);
+        public Block() {
+            width = getWidth() / REC_WIDTH; //System.out.println("Block constructor");
+            height = getHeight() / REC_HEIGHT;
+            heightOffset = height / 2;
+            x = getWidth() / X_POS;
+            y = getHeight() / Y_POS - heightOffset;
+            rec = new Rectangle2D.Double(x, y, width, height);
         }
 
+        /**
+         * 
+         */
         public double getX() {
             return x;
         }
 
+        /**
+         * 
+         */
         public double getY() {
             return y;
         }
 
+        /**
+         * 
+         */
         public void move(double dx) {
-            x += dx;
+            x += dx; System.out.println("supoposed to move");
             repaint();
         }
 
+        /**
+         * 
+         */
         public boolean reachedEnd() {
             if ((x + width) >= getWidth())
                 return true;
             else return false;
         }
 
+        /**
+         * 
+         */
         public void draw(Graphics2D g) {
-            g.setColor(Color.BLACK);
-            g.fill(rec);
+            g.setColor(Color.BLACK); 
+            g.fill(rec);System.out.println("Draw");
         }
     }
 }
